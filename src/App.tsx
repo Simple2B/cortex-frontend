d24b59682d47a69270e7c2ab4eb4aa04f1dc1678
Julia <mjulia1794@gmail.com>
2021-09-16 09:33:53 +0300

    bug fixing

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,80 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Redirect,
 } from "react-router-dom";
 import './App.css';
 import Kiosk from './components/Kiosk/Kiosk';
 import Login from './components/Login/Login';
 import PasswordChoose from './components/PasswordChoose/PasswordChoose';
+import PatientRegistration from './components/PatientRegistration/PatientRegistration';
 import Patients from './components/Patients/Patients';
 import Queue from './components/Queue/Queue';
 import Reports from './components/Reports/Reports';
+import ProtectedRoute, { ProtectedRouteProps } from './ProtectedRoute';
+import { useTypedSelector } from './redux/useTypeSelector';
 
 function App() {
+  const loggedIn = useTypedSelector((state) => state.auth.loggedIn);
+
+  const defaultProtectedRouteProps: ProtectedRouteProps = {
+    isAuthenticated: loggedIn,
+    authenticationPath: "/login",
+  };
+
   return (
-    <div className="App">
+    <div className="App" >
       <Router>
         <Switch>
+          {/* <Route exact path="/">
+            {loggedIn ? (
+              <Redirect
+                to={{
+                  pathname: "/queue",
+                  state: { path: "/" },
+                }}
+              />
+            ) : (
+                <Redirect
+                  to={{
+                    pathname: "/login",
+                    state: { path: "/" },
+                  }}
+                />
+              )}
+          </Route>
+
+          <Route exact path="/login">
+            {!loggedIn ? <Login /> : <Redirect to={"/"} />}
+          </Route>
+
+          <ProtectedRoute
+            {...defaultProtectedRouteProps}
+            path="/kiosk"
+            component={Kiosk}
+          />
+          <ProtectedRoute
+            {...defaultProtectedRouteProps}
+            path="/reports"
+            component={Reports}
+          />
+          <ProtectedRoute
+            {...defaultProtectedRouteProps}
+            path="/patients"
+            component={Patients}
+          />
+          <ProtectedRoute
+            {...defaultProtectedRouteProps}
+            path="/queue"
+            component={Queue}
+          /> */}
+
+
           <Route path="/password-choose">
             <PasswordChoose />
+          </Route>
+          <Route path="/patient-registration">
+            <PatientRegistration />
           </Route>
           <Route path="/kiosk">
             <Kiosk />
@@ -39,7 +97,7 @@ function App() {
         </Switch>
       </Router>
 
-    </div>
+    </div >
   );
 }
 
