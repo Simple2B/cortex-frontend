c8594e45512871cf0a581752f19c0bea1b776486
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-10-02 20:20:51 +0000

    logic of add user to queue

--- src/components/Queue/Queue.tsx
+++ src/components/Queue/Queue.tsx
@@ -4,19 +4,28 @@ import { patientsList } from '../../fakeBase'
 import NavBar from '../NavBar/NavBar';
 import './queue.css'
 
+interface User {
+  name: string,
+  lastName: string,
+}
+
 
 export default function Queue(): ReactElement {
-  const [queue, setQueue] = useState<any[]>([])
+  const [queue, setQueue] = useState<User[]>([]);
+  const [patients, setPatients] = useState(patientsList)
 
-  const addPatient = (patient: any) => {
-    setQueue((prev) => {
-      return [...prev, patient];
-    });
-  }
+  const addPatient = (patient: User) => {
+    setQueue((prev: User[]) => [...prev, patient]);
+  };
 
-  const patientComponents = patientsList.map(item => (
-    <div className="queue_list" onClick={() => {
-      addPatient({ name: item.name, lastName: item.lastName })
+  const patientComponents = patients.map((item, index )=> (
+    <div className="queue_list" onClick={(e: any) => {
+      const copyListPatients = [...patients];
+      const patient_target = e.target.innerText.split(",");
+      if (patient_target[0].toLowerCase().trim() === copyListPatients[index].lastName.toLowerCase().trim() && patient_target[1].toLowerCase().trim() === copyListPatients[index].name.toLowerCase().trim()) {
+        addPatient({ name: item.name, lastName: item.lastName });
+        setPatients(patients.filter(patient => patient !== item));
+      }
     }}>{item.lastName}, {item.name}</div>
   ))
 
@@ -34,9 +43,11 @@ export default function Queue(): ReactElement {
       </div>
       <div className="queue">
         <h1 className="queue_title">The Queue</h1>
-        {queue.map(item => (
-          <div className="queue_list" key={item}>{item.lastName}, {item.name}</div>
-        ))}
+        {
+          queue.map(item => (
+            <div className="queue_list">{item.lastName}, {item.name}</div>
+          ))
+        }
         <Popup trigger={<button className="queue_add_button">+Add new</button>} modal>
           <div className="modal_window">{patientComponents}</div>
         </Popup>
