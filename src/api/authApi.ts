33ba4ac385d69915d899cdf90c35caabfe71a65b
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-10-04 11:00:32 +0000

    delete commits

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -43,20 +43,6 @@ const formatRequestBodyApiKey = (password: string, api_key: string) => {
 };
 
 export const authApi = {
-  // login: async (email: string, password: string) => {
-  //   const response = await authInstance
-  //     .post('api/auth/sign_in', formatRequestBody(email, password))
-  //     .then((res: AxiosResponse<ILoginResponse>) => {
-  //       console.log("POST [/auth/sign_in] response received successfully");
-  //       return res.data;
-  //     })
-  //     .catch((error: AxiosError<ILoginResponse>) => {
-  //       // place to handle errors and rise custom errors
-  //       console.log(`POST [api/auth/sign_in] error message: ${error.message}`);
-  //       throw error.message;
-  //     });
-  //   return response;
-  // },
 
   login: async (email: string, password: string): Promise<ILoginResponse> => {
     try {
@@ -83,21 +69,6 @@ export const authApi = {
       throw error.message;
     }
   },
-
-  // registrationClient: async (data: DataClient ) => {
-  //   const response = await authInstance
-  //     .post('api/client/registration', data)
-  //     .then((res: AxiosResponse<ILoginResponse>) => {
-  //       console.log(`POST [api/client/registration/${data}] response received successfully`);
-  //       return res.data;
-  //     })
-  //     .catch((error: AxiosError<ILoginResponse>) => {
-  //       // place to handle errors and rise custom errors
-  //       console.log(`POST [api/client/registration/${data}] error message: ${error.message}`);
-  //       throw error.message;
-  //     });
-  //   return response;
-  // },
 
   registrationClient: async (data: DataClient): Promise<void> => {
     try {
