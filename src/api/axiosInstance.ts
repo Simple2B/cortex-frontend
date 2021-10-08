398f5fc0282010d3ef172c96aa82d7387a50d90e
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-10-08 08:10:38 +0000

    instance() axios

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,16 +2,19 @@ import axios, { AxiosInstance } from "axios";
 import domain from "./domain.json";
 
 
-const token = localStorage.getItem("token") ?? "";
-export const instance: AxiosInstance = axios.create({
-  baseURL: domain.REACT_DOMAIN,
-  headers: {
-    Authorization: `Bearer ${token}`,
-    // "Content-Type": "application/json; charset=utf-8",
-    'Access-Control-Allow-Origin' : '*',
-    // 'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-  },
-});
+
+export const instance = (): AxiosInstance => {
+  const token = localStorage.getItem("token") ?? "";
+  return axios.create({
+    baseURL: domain.REACT_DOMAIN,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      // "Content-Type": "application/json; charset=utf-8",
+      'Access-Control-Allow-Origin' : '*',
+      // 'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+    },
+  });
+};
 
 export const authInstance: AxiosInstance = axios.create({
   baseURL: domain.REACT_DOMAIN,
