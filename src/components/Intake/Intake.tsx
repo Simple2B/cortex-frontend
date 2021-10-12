83d89c1e3610dde31c1a5b82a043f24fe8f7e9d1
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-10-12 20:30:05 +0000

    style intake for full screen

--- src/components/Intake/Intake.tsx
+++ src/components/Intake/Intake.tsx
@@ -3,6 +3,8 @@ import './intake.css';
 import { NavLink } from 'react-router-dom';
 import NavBar from '../NavBar/NavBar';
 import MenuInfoPatient from '../MenuInfoPatient/MenuInfoPatient';
+import { ReactComponent as IntakeDashboard } from '../../images/intake_dashboard.svg';
+import { ReactComponent as IntakeAlpha } from '../../images/intake_alpha.svg';
 
 
 export default function Intake(): ReactElement {
@@ -11,6 +13,73 @@ export default function Intake(): ReactElement {
     <>
         <NavBar />
         <MenuInfoPatient/>
+        <div className="containerIntakeContent">
+
+          <div className="coherence">
+            <div className="coherenceTitleText">Coherence</div>
+            <div className="coherenceDashboard">
+                <IntakeDashboard/>
+            </div>
+            <div className="coherenceBtn">
+              <div className="coherenceBtn_circles">
+                <div className="coherenceBtn_circle"></div>
+                <div className="coherenceBtn_circle"></div>
+                <div className="coherenceBtn_circle coherenceBtn_circleActive"></div>
+              </div>
+              <div className="coherenceBtn_complete">
+                Complete
+              </div>
+            </div>
+          </div>
+
+          <div className="intakeInfo">
+            <div className="intakeInfoText">
+
+              <div className="intakeInfoText_health">
+                <i className="fas fa-times"/>
+                <div className="intakeInfoText_healthTitle">Intake</div>
+                <div className="intakeInfoText_healthBtn">
+                  <div className="intake_btn">
+                    Consult
+                  </div>
+                  <div className="intake_btn intake_btnActive">
+                    Health HX
+                  </div>
+                  <div className="intake_btn">
+                    Family HX
+                  </div>
+                </div>
+              </div>
+              <div className="intakeInfoText_results">
+                <div className="results">
+                </div>
+                <div className="results">
+
+                </div>
+                <div className="results">
+
+                </div>
+              </div>
+
+            </div>
+
+            <div className="intakeInfoAlpha">
+              <div className="intakeInfoAlpha_text">
+                Alpha
+              </div>
+
+              <div className="intakeInfoAlpha_letters">
+                <div className="letter">R</div>
+                <div className="letter">L</div>
+              </div>
+
+              <div className="intakeInfoAlpha_dashboard">
+                <IntakeAlpha/>
+              </div>
+
+            </div>
+          </div>
+        </div>
     </>
   )
 }
