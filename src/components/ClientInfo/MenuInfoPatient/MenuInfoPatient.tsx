0a484fca733add38f956b5c0d794ce8aa037645b
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-10-14 22:58:27 +0000

    menu for info page client

---
+++ src/components/ClientInfo/MenuInfoPatient/MenuInfoPatient.tsx
@@ -0,0 +1,51 @@
+import React, { ReactElement } from 'react';
+import { useLocation } from "react-router-dom";
+import './menuInfoPatient.css';
+import { NavLink } from 'react-router-dom';
+
+
+interface MyProps {
+  api_key: string
+  name: string
+}
+
+
+export default function MenuInfoPatient(props: MyProps): ReactElement {
+
+      //assigning location variable
+      const location = useLocation();
+
+      //destructuring pathname from location
+      const { pathname } = location;
+
+      //Javascript split method to get the name of the path in array
+      const splitLocation = pathname.split("/");
+
+      console.log("props.name ", props.name );
+
+  return (
+    <div className="menuInfoPatient_container">
+        <div className="menuInfoPatient">
+            <div className={splitLocation[splitLocation.length - 1] === "#" ? "active" : "menuInfoPatientItem"}>
+              {/* test data props */}
+              <NavLink to="#">{ props.name === '' || props.name === null ? 'JOHN' : props.name }</NavLink>
+            </div>
+            <div className={splitLocation[splitLocation.length - 1] === "#" ? "active" : "menuInfoPatientItem"}>
+              <NavLink to="#">Care Plan</NavLink>
+            </div>
+            <div className={splitLocation[splitLocation.length - 1] === "#" ? "active" : "menuInfoPatientItem"}>
+              <NavLink to="#">Notes</NavLink>
+            </div>
+            <div className={splitLocation[splitLocation.length - 1] === 'intake' ? "active" : "menuInfoPatientItem"}>
+              <NavLink to={`/${props.api_key}/intake`}>Intake</NavLink>
+            </div>
+            <div className={splitLocation[splitLocation.length - 1] === "#" ? "active" : "menuInfoPatientItem"}>
+              <NavLink to="#">Report</NavLink>
+            </div>
+            <div className={splitLocation[splitLocation.length - 1] === "account" ? "active" : "menuInfoPatientItem"}>
+              <NavLink to={`/${props.api_key}/account`}>Account</NavLink>
+            </div>
+        </div>
+    </div>
+  )
+}
