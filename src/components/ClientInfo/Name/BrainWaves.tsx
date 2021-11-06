b6f4b29a0738f06e1d56664586a33ea64f46afea
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-11-06 23:23:28 +0000

    dashboard components

---
+++ src/components/ClientInfo/Name/BrainWaves.tsx
@@ -0,0 +1,28 @@
+import React, { ReactElement } from 'react';
+import "./name.sass";
+import arrowRight  from "../../../images/arrowRight.svg";
+import  arrowLeft  from "../../../images/arrowLeft.svg";
+import brainWaves from "../../../images/brainWaves.svg";
+
+
+export default function BrainWaves(): ReactElement {
+
+  return (
+    <>
+        <div className="title">Brain Waves</div>
+        <div className="overAroused">
+            <div className="overAroused_content">
+                <div className="arrowLeft">
+                    <img src={arrowLeft} alt="arrowLeft" />
+                </div>
+                <div className="nameDashboard">
+                    <img src={brainWaves} alt="brainWaves" />
+                </div>
+                <div className="arrowRight">
+                    <img src={arrowRight} alt="arrowRight" />
+                </div>
+            </div>
+        </div>
+    </>
+  )
+}
