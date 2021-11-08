e17df22829b8344e2667a6324aa173aab63c9e68
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-11-08 22:40:15 +0000

    dashboard style

--- src/components/ClientInfo/Name/Name.tsx
+++ src/components/ClientInfo/Name/Name.tsx
@@ -67,97 +67,95 @@ export default function Name(): ReactElement {
 
   return (
     <>
-        <div className="nameContainer">
-            <div className="nameContainer_arousal">
+      <div className="nameContainer_arousal">
 
-                {
-                    dashboard === 'arousal' && <Arousal/>
-                    ||
-                    dashboard === 'brainWaves' && <BrainWaves/>
-                    ||
-                    dashboard === 'coherence' && <Coherence/>
-                }
+            {
+                dashboard === 'arousal' && <Arousal/>
+                ||
+                dashboard === 'brainWaves' && <BrainWaves/>
+                ||
+                dashboard === 'coherence' && <Coherence/>
+            }
 
-                <div className="containerComplete">
-                  <div className="btn_circles">
-                    <div className={`${dashboard === 'arousal' ? "btn_circleActive" : "btn_circle"}`} onClick={() => setDashboard("arousal")}></div>
-                    <div className={`${dashboard === 'brainWaves' ? "btn_circleActive" : "btn_circle"}`} onClick={() => setDashboard("brainWaves")}></div>
-                    <div className={`${dashboard === 'coherence' ? "btn_circleActive" : "btn_circle"}`} onClick={() => setDashboard("coherence")}></div>
-                  </div>
-                  <div className="coherenceBtn_complete" onClick={() => {
-                        clientApi.completeClient({"api_key": api_key,
-                        "rougue_mode": false, "place_in_queue": client.place_in_queue});
-                        history.push('/queue');
-                        console.log("client" , {"api_key": api_key,
-                        "rougue_mode": false, "first_name": client.firstName, "place_in_queue": client.place_in_queue})
-                  }}>
-                    Complete
-                  </div>
-                </div>
+          <div className="containerComplete">
+            <div className="btn_circles">
+              <div className={`${dashboard === 'arousal' ? "btn_circleActive" : "btn_circle"}`} onClick={() => setDashboard("arousal")}></div>
+              <div className={`${dashboard === 'brainWaves' ? "btn_circleActive" : "btn_circle"}`} onClick={() => setDashboard("brainWaves")}></div>
+              <div className={`${dashboard === 'coherence' ? "btn_circleActive" : "btn_circle"}`} onClick={() => setDashboard("coherence")}></div>
             </div>
+            <div className="coherenceBtn_complete" onClick={() => {
+                  clientApi.completeClient({"api_key": api_key,
+                  "rougue_mode": false, "place_in_queue": client.place_in_queue});
+                  history.push('/queue');
+                  console.log("client" , {"api_key": api_key,
+                  "rougue_mode": false, "first_name": client.firstName, "place_in_queue": client.place_in_queue})
+            }}>
+              Complete
+            </div>
+          </div>
+      </div>
 
-            <div className="nameContainer_brain">
+      <div className="nameContainer_brain">
 
-              <div className="nameContainer_brainContent">
-                <div className="brain_btns">
-                  <div className="btn">
-                    <div className="btn_Title">Atlas</div>
-                    <div className="btnContainer">
-                      <div onClick={handleChangeBtn} className={activeBtnAtlas == "L" ? "btnActive" : "name_btn"}>L</div>
-                      <div onClick={handleChangeBtn} className={activeBtnAtlas == "X" ? "btnActive" : "name_btn"}>X</div>
-                      <div onClick={handleChangeBtn} className={activeBtnAtlas == "R" ? "btnActive" : "name_btn"}>R</div>
-                    </div>
-                  </div>
-                  <div className="btn">
-                    <div className="btn_Title">ShortLeg</div>
-                    <div className="btnContainer">
-                      <div onClick={handleChangeBtnShortLeg} className={activeBtnShortLeg == "L" ? "btnActive" : "name_btn"}>L</div>
-                      <div onClick={handleChangeBtnShortLeg} className={activeBtnShortLeg == "X" ? "btnActive" : "name_btn"}>X</div>
-                      <div onClick={handleChangeBtnShortLeg} className={activeBtnShortLeg == "R" ? "btnActive" : "name_btn"}>R</div>
-                    </div>
-                  </div>
-                  <div className="btn containerRogueMode">
-                    <div className="btn_Title titleRogueMode">Rogue Mode</div>
-                    <div className="btnContainer btnRogueMode">
-                      <div onClick={handleChangeBtnRogueMode} className={activeBtnRogueMode == "on" ? "btnActive" : "name_btn"}>on</div>
-                      <div onClick={handleChangeBtnRogueMode} className={activeBtnRogueMode == "off" ? "btnActive" : "name_btn"}>off</div>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="brain">
-                    <Brain/>
-                </div>
-
-                <div className="intakeInfoText_results">
-                  <div className="results">
-                    <div>63bpm</div>
-                    <div>HR</div>
-                  </div>
-                  <div className="results">
-                    <div>10</div>
-                    <div>Resp</div>
-                  </div>
-                  <div className="results">
-                    <div>98%</div>
-                    <div>SpO2</div>
-                  </div>
-                </div>
-              </div>
-              <div className="alphaContainer">
-                <div className="alphaContainer_text">
-                  Alpha
-                </div>
-                <div className="alphaContainer_letters">
-                  <div className="letter">R</div>
-                  <div className="letter">L</div>
-                </div>
-                <div className="alphaContainer_dashboard">
-                  <IntakeAlpha/>
-                </div>
+        <div className="nameContainer_brainContent">
+          <div className="brain_btns">
+            <div className="btn">
+              <div className="btn_Title">Atlas</div>
+              <div className="btnContainer">
+                <div onClick={handleChangeBtn} className={activeBtnAtlas == "L" ? "btnActive" : "name_btn"}>L</div>
+                <div onClick={handleChangeBtn} className={activeBtnAtlas == "X" ? "btnActive" : "name_btn"}>X</div>
+                <div onClick={handleChangeBtn} className={activeBtnAtlas == "R" ? "btnActive" : "name_btn"}>R</div>
               </div>
             </div>
+            <div className="btn">
+              <div className="btn_Title">ShortLeg</div>
+              <div className="btnContainer">
+                <div onClick={handleChangeBtnShortLeg} className={activeBtnShortLeg == "L" ? "btnActive" : "name_btn"}>L</div>
+                <div onClick={handleChangeBtnShortLeg} className={activeBtnShortLeg == "X" ? "btnActive" : "name_btn"}>X</div>
+                <div onClick={handleChangeBtnShortLeg} className={activeBtnShortLeg == "R" ? "btnActive" : "name_btn"}>R</div>
+              </div>
+            </div>
+            <div className="btn containerRogueMode">
+              <div className="btn_Title titleRogueMode">Rogue Mode</div>
+              <div className="btnContainer btnRogueMode">
+                <div onClick={handleChangeBtnRogueMode} className={activeBtnRogueMode == "on" ? "btnActive" : "name_btn"}>on</div>
+                <div onClick={handleChangeBtnRogueMode} className={activeBtnRogueMode == "off" ? "btnActive" : "name_btn"}>off</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="brain">
+              <Brain/>
+          </div>
+
+          <div className="intakeInfoText_results">
+            <div className="results">
+              <div>63bpm</div>
+              <div>HR</div>
+            </div>
+            <div className="results">
+              <div>10</div>
+              <div>Resp</div>
+            </div>
+            <div className="results">
+              <div>98%</div>
+              <div>SpO2</div>
+            </div>
+          </div>
         </div>
+        <div className="alphaContainer">
+          <div className="alphaContainer_text">
+            Alpha
+          </div>
+          <div className="alphaContainer_letters">
+            <div className="letter">R</div>
+            <div className="letter">L</div>
+          </div>
+          <div className="alphaContainer_dashboard">
+            <IntakeAlpha/>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
