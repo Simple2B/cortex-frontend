d458c6fb4675aca503f023fb6771c16bfa14f705
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-11-10 21:07:39 +0000

    fix style

--- src/components/ClientInfo/Account/Account.tsx
+++ src/components/ClientInfo/Account/Account.tsx
@@ -40,7 +40,11 @@ export default function Account(): ReactElement {
           <div className="clientInfo">
             <div className="clientInfo_tittle">Client info</div>
             <div className="clientInfoAccount">
-              <div className="info_title">Name <div className="clientInfo_text">{client.firstName} {client.lastName}</div></div>
+              <div className="infoContainer">
+                <div className="info_title">Name </div>
+                <div className="clientInfo_text">{client.firstName} {client.lastName}</div>
+              </div>
+
               <div className="info"><div>DOB: </div><div className="clientInfo_text">{client.birthday}</div></div>
               <div className="info"><div>Address: </div><div className="clientInfo_text">{client.address}</div></div>
               <div className="info"><div>City: </div><div className="clientInfo_text">{client.city}</div></div>
