eba8cd94ea8ce0d7bb41a82ce593caa65e4612a4
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-11-12 13:13:16 +0000

    report component

--- src/components/ClientInfo/AccountReport/AccountReport.tsx
+++ src/components/ClientInfo/AccountReport/AccountReport.tsx
@@ -1,12 +1,23 @@
 import React, { ReactElement } from 'react';
+import "./AccountReport.sass";
 
 
 export function AccountReport(): ReactElement {
 
 
   return (
-    <div style={{"textAlign": "center"}}>
-        AccountReport
+    <div className="containerReportAccount">
+      <div className="reportAccountBtn">
+        New Test
+      </div>
+      <div className="reportAccountTestTime">
+
+        <div>March 12, 2020</div>
+        <div>February 14, 2020</div>
+        <div>January 13, 2020</div>
+
+      </div>
     </div>
+
   )
 };
