69d9502ff04f11d3dd6bdc87abd382b06bf0a545
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-11-17 12:17:22 +0000

    time and btn in modal window

--- src/components/ClientInfo/AccountReport/AccountReport.tsx
+++ src/components/ClientInfo/AccountReport/AccountReport.tsx
@@ -1,11 +1,9 @@
-import React, { ReactElement, useEffect } from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
-import { useActions } from '../../../redux/useActions';
+import React, { ReactElement, useEffect } from "react";
+import { useHistory, useLocation } from "react-router-dom";
+import { useActions } from "../../../redux/useActions";
 import "./AccountReport.sass";
 
-
 export function AccountReport(): ReactElement {
-
   const location = useLocation();
   const splitLocation = location.pathname.split("/");
   const api_key = splitLocation[splitLocation.length - 2];
@@ -14,24 +12,21 @@ export function AccountReport(): ReactElement {
 
   const { dashboardUrl } = useActions();
 
-  useEffect(() => {
-
-  }, [api_key]);
-
+  useEffect(() => {}, [api_key]);
 
   return (
     <div className="containerReportAccount">
-      <div className="reportAccountBtn" onClick={() => history.push(`/${api_key}/start`)}>
+      <div
+        className="reportAccountBtn"
+        onClick={() => history.push(`/${api_key}/start`)}
+      >
         New Test
       </div>
       <div className="reportAccountTestTime">
-
         <div>March 12, 2020</div>
         <div>February 14, 2020</div>
         <div>January 13, 2020</div>
-
       </div>
     </div>
-
-  )
-};
+  );
+}
--- src/components/ClientInfo/AccountReport/AccountReportStart.tsx
+++ src/components/ClientInfo/AccountReport/AccountReportStart.tsx
@@ -1,12 +1,12 @@
-import React, { ReactElement, useEffect, useState } from 'react';
+import React, { ReactElement, useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
-import { Client, ClientDefault } from '../../../api/clientApi';
-import {instance} from '../../../api/axiosInstance';
-import "../Name/name.sass";
-import { ReactComponent as IntakeAlpha } from '../../../images/intake_alpha.svg';
-import { ReactComponent as Brain } from '../../../images/brain.svg';
-import Dashboards from '../Dashboard/Dashboards';
-
+import { Client, ClientDefault } from "../../../api/clientApi";
+import { instance } from "../../../api/axiosInstance";
+// import "../Name/name.sass";
+import "./AccountReport.sass";
+import { ReactComponent as IntakeAlpha } from "../../../images/intake_alpha.svg";
+import { ReactComponent as Brain } from "../../../images/brain.svg";
+import Dashboards from "../Dashboard/Dashboards";
 
 export default function AccountReportStart(): ReactElement {
   const location = useLocation();
@@ -16,46 +16,43 @@ export default function AccountReportStart(): ReactElement {
 
   const [activeBtnRogueMode, setActiveBtnRogueMode] = useState("off");
 
-
   const getClient = async () => {
     try {
-      const response = await instance()
-      .get(`api/client/client_intake/${api_key}`);
+      const response = await instance().get(
+        `api/client/client_intake/${api_key}`
+      );
       console.log("GET: client_intake name => ", response.data);
       setClient(response.data);
-      return response.data
+      return response.data;
     } catch (error: any) {
-      console.log('GET: error message get_client_intake name =>  ', error.message);
-      console.log('error response data get_client_intake name => ', error.response.data);
+      console.log(
+        "GET: error message get_client_intake name =>  ",
+        error.message
+      );
+      console.log(
+        "error response data get_client_intake name => ",
+        error.response.data
+      );
       throw new Error(error.message);
-    };
-  }
+    }
+  };
 
   useEffect(() => {
-    getClient()
+    getClient();
   }, [api_key]);
 
   useEffect(() => {
     setActiveBtnRogueMode(activeBtnRogueMode);
-    // if (props.activeBtnRogueMode === "on") {
-    //   history.push(`/nameOn/${dashboard}`);
-    // }
   }, [activeBtnRogueMode]);
-
-
-  console.log("activeBtnRogueMode", activeBtnRogueMode);
-
 
   return (
     <>
-      <Dashboards activeBtnRogueMode={activeBtnRogueMode}/>
+      <Dashboards activeBtnRogueMode={activeBtnRogueMode} />
 
-      <div className="nameContainer_brain">
-
+      <div className="accountReportStart_nameContainer_brain">
         <div className="nameContainer_brainContent">
-
           <div className="brain">
-              <Brain/>
+            <Brain />
           </div>
 
           <div className="intakeInfoText_results">
@@ -74,18 +71,25 @@ export default function AccountReportStart(): ReactElement {
           </div>
         </div>
         <div className="alphaContainer">
-          <div className="alphaContainer_text">
-            Alpha
-          </div>
+          <div className="alphaContainer_text">Alpha</div>
           <div className="alphaContainer_letters">
             <div className="letter">R</div>
             <div className="letter">L</div>
           </div>
           <div className="alphaContainer_dashboard">
-            <IntakeAlpha/>
+            <IntakeAlpha />
+          </div>
+        </div>
+      </div>
+
+      <div className="accountReportStart_modalWindow">
+        <div className="modalWindow_content">
+          <div className="content">
+            <div className="modalWindow_time">07:47</div>
+            <div className="modalWindow_btnStart">Start</div>
           </div>
         </div>
       </div>
     </>
-  )
-};
+  );
+}
