5a759de3296e43cbebc0e7b4e8f3473cb7f9cb10
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-11-17 15:35:07 +0000

    toggle btn

--- src/components/ClientInfo/ViewReport/ViewReport.tsx
+++ src/components/ClientInfo/ViewReport/ViewReport.tsx
@@ -11,6 +11,8 @@ export default function ViewReport(): ReactElement {
   const api_key = splitLocation[splitLocation.length - 2];
   const [client, setClient] = useState<Client>(ClientDefault);
 
+  const [activeBtn, setActiveBtn] = useState<string>("Brainwaves");
+
   const getClient = async () => {
     try {
       const response = await instance().get(
@@ -36,6 +38,10 @@ export default function ViewReport(): ReactElement {
     getClient();
   }, [api_key]);
 
+  const handleChangeBtn = (e: any) => {
+    setActiveBtn(e.currentTarget.innerHTML);
+  };
+
   return (
     <div className="containerViewReport">
       <div className="containerViewReport_dashboards">
@@ -57,10 +63,30 @@ export default function ViewReport(): ReactElement {
         </div>
       </div>
       <div className="containerViewReport_toggleBtn">
-        <div className="btn activeBtn">Brainwaves</div>
-        <div className="btn">HRV</div>
-        <div className="btn">Resp/HR/SpO2</div>
-        <div className="btn">Care plan</div>
+        <div
+          onClick={handleChangeBtn}
+          className={activeBtn == "Brainwaves" ? "activeBtn" : "btn"}
+        >
+          Brainwaves
+        </div>
+        <div
+          onClick={handleChangeBtn}
+          className={activeBtn == "HRV" ? "activeBtn" : "btn"}
+        >
+          HRV
+        </div>
+        <div
+          onClick={handleChangeBtn}
+          className={activeBtn == "Resp/HR/SpO2" ? "activeBtn" : "btn"}
+        >
+          Resp/HR/SpO2
+        </div>
+        <div
+          onClick={handleChangeBtn}
+          className={activeBtn == "Care plan" ? "activeBtn" : "btn"}
+        >
+          Care plan
+        </div>
       </div>
     </div>
   );
