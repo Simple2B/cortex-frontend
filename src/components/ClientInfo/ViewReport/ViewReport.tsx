12d1e0cc201643a13a569baa786834cc97362f05
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-11-18 10:58:43 +0000

    report care plan

--- src/components/ClientInfo/ViewReport/ViewReport.tsx
+++ src/components/ClientInfo/ViewReport/ViewReport.tsx
@@ -1,9 +1,91 @@
 import React, { ReactElement, useEffect, useRef, useState } from "react";
 import { useLocation } from "react-router-dom";
 import "./ViewReport.sass";
-import Dashboards from "../Dashboard/Dashboards";
+import DatePicker from "react-datepicker";
+import Select, { components } from "react-select";
+import { ReactComponent as Arrow } from "../../../images/arrow.svg";
 import { Client, ClientDefault } from "../../../api/clientApi";
 import { instance } from "../../../api/axiosInstance";
+
+interface IDataReport {
+  startDate: Date;
+  endDate: Date;
+  type: string;
+}
+
+interface IDataReportToBack {
+  startDate: string;
+  endDate: string;
+  type: string;
+}
+
+const customStyles: any = {
+  option: (provided: any, state: { isSelected: any }) => ({
+    ...provided,
+    border: "1px solid #fff",
+    background: "black",
+    fontSize: "25px",
+    color: "white",
+    padding: 20,
+  }),
+
+  control: () => ({
+    position: "relative",
+    width: 290,
+    height: 61,
+  }),
+
+  menu: () => ({
+    background: "black",
+    marginTop: "-12px",
+  }),
+
+  valueContainer: () => ({
+    position: "relative",
+    border: "1px solid #aef7ff",
+    fontSize: "25px",
+    color: "#aef7ff",
+    borderRadius: "10px",
+    zIndex: "100",
+    background: "black",
+  }),
+
+  indicatorsContainer: () => ({
+    position: "absolute",
+    zIndex: "1000",
+    top: "20px",
+    right: "20px",
+  }),
+
+  input: () => ({
+    height: 61,
+    paddingLeft: "20px",
+    lineHeight: "61px",
+  }),
+
+  placeholder: () => ({
+    paddingLeft: "20px",
+    position: "absolute",
+    lineHeight: "61px",
+  }),
+
+  singleValue: (provided: any, state: { isDisabled: any }) => {
+    const transition = "opacity 300ms";
+    const color = "#aef7ff";
+    const fontSize = "25px";
+    const paddingLeft = "20px";
+
+    return { ...provided, transition, color, fontSize, paddingLeft };
+  },
+};
+
+const DropdownIndicator = (props: any) => {
+  return (
+    <components.DropdownIndicator {...props}>
+      <Arrow />
+    </components.DropdownIndicator>
+  );
+};
 
 export default function ViewReport(): ReactElement {
   const location = useLocation();
@@ -12,6 +94,35 @@ export default function ViewReport(): ReactElement {
   const [client, setClient] = useState<Client>(ClientDefault);
 
   const [activeBtn, setActiveBtn] = useState<string>("Brain");
+
+  const [typeCaraPlan, setTypeCaraPlan] = useState<any>(null);
+  const [typeFrequency, setTypeFrequency] = useState<any>(null);
+  const [typeProgressTest, setTypeProgressTest] = useState<any>(null);
+  const [file, setFile] = useState<string | any>(null);
+
+  const optionsCarePlane = [
+    { value: "Care plan option", label: "Care plan option" },
+    { value: "Care plan option", label: "Care plan option" },
+    { value: "Care plan option", label: "Care plan option" },
+    { value: "Care plan option", label: "Care plan option" },
+    { value: "Care plan option", label: "Care plan option" },
+  ];
+
+  const optionsFrequency = [
+    { value: "Frequency option", label: "Frequency option" },
+    { value: "Frequency option", label: "Frequency option" },
+    { value: "Frequency option", label: "Frequency option" },
+    { value: "Frequency option", label: "Frequency option" },
+    { value: "Frequency option", label: "Frequency option" },
+  ];
+
+  const optionsProgressTest = [
+    { value: "Progress option", label: "Progress option" },
+    { value: "Progress option", label: "Progress option" },
+    { value: "Progress option", label: "Progress option" },
+    { value: "Progress option", label: "Progress option" },
+    { value: "Progress option", label: "Progress option" },
+  ];
 
   const getClient = async () => {
     try {
@@ -42,87 +153,128 @@ export default function ViewReport(): ReactElement {
     setActiveBtn(e.currentTarget.innerHTML);
   };
 
+  const handleSelectCaraPlan = (type: string) => {
+    setTypeCaraPlan(type);
+  };
+
+  const handleSelectFrequency = (type: string) => {
+    setTypeFrequency(type);
+  };
+
+  const handleSelectProgressTest = (type: string) => {
+    setTypeProgressTest(type);
+  };
+
   return (
     <div className="containerViewReport">
-      {activeBtn === "Brain" && (
-        <div className="containerViewReport_dashboards">
-          <div className="exampleBrain">exampleBrain</div>
-          <div className="patientBrain">patientBrain</div>
-        </div>
-      )}
+      <div className="containerViewReport_report">
+        {activeBtn === "Brain" && (
+          <div className="containerViewReport_dashboards">
+            <div className="exampleBrain">exampleBrain</div>
+            <div className="patientBrain">patientBrain</div>
+          </div>
+        )}
 
-      {activeBtn === "Brainwaves" && (
-        <div className="containerViewReport_dashboards">
-          <div className="containerDashboard">
-            <div className="dashboard">dashboard</div>
-            <div className="title">BETA</div>
+        {activeBtn === "Brainwaves" && (
+          <div className="containerViewReport_dashboards">
+            <div className="containerDashboard">
+              <div className="dashboard">dashboard</div>
+              <div className="title">BETA</div>
+            </div>
+            <div className="containerDashboard">
+              <div className="dashboard">dashboard</div>
+              <div className="title">SMR</div>
+            </div>
+            <div className="containerDashboard">
+              <div className="dashboard">dashboard</div>
+              <div className="title">ALPHA</div>
+            </div>
+            <div className="containerDashboard">
+              <div className="dashboard">dashboard</div>
+              <div className="title">THETA</div>
+            </div>
           </div>
-          <div className="containerDashboard">
-            <div className="dashboard">dashboard</div>
-            <div className="title">SMR</div>
-          </div>
-          <div className="containerDashboard">
-            <div className="dashboard">dashboard</div>
-            <div className="title">ALPHA</div>
-          </div>
-          <div className="containerDashboard">
-            <div className="dashboard">dashboard</div>
-            <div className="title">THETA</div>
-          </div>
-        </div>
-      )}
+        )}
 
-      {activeBtn === "HRV" && (
-        <div className="containerViewReport_dashboards">
-          <div className="containerDashboard">
-            <div className="dashboard">dashboard</div>
-            <div className="title">Very Low Frequency</div>
+        {activeBtn === "HRV" && (
+          <div className="containerViewReport_dashboards">
+            <div className="containerDashboard">
+              <div className="dashboard">dashboard</div>
+              <div className="title">Very Low Frequency</div>
+            </div>
+            <div className="containerDashboard">
+              <div className="dashboard">dashboard</div>
+              <div className="title">Low Frequency</div>
+            </div>
+            <div className="containerDashboard">
+              <div className="dashboard">dashboard</div>
+              <div className="title">High Frequency</div>
+            </div>
           </div>
-          <div className="containerDashboard">
-            <div className="dashboard">dashboard</div>
-            <div className="title">Low Frequency</div>
-          </div>
-          <div className="containerDashboard">
-            <div className="dashboard">dashboard</div>
-            <div className="title">High Frequency</div>
-          </div>
-        </div>
-        // <div className="containerViewReport_dashboards">
-        //   <div className="containerDashboard"></div>
-        //   <div className="containerDashboard">
-        //     <div className="dashboard">dashboard</div>
-        //     <div className="title">Heart Rate</div>
-        //   </div>
-        //   <div className="containerDashboard"></div>
-        // </div>
-      )}
+        )}
 
-      {activeBtn === "Care plan" && (
-        <div className="containerViewReport_dashboards">Care plan</div>
-      )}
+        {activeBtn === "Care plan" && (
+          <div className="containerViewReport_dashboards">
+            <div className="reports">
+              <div className="reportsGeneration">
+                <div className="reportTypeSelector">
+                  <Select
+                    components={{ DropdownIndicator }}
+                    placeholder={"Care plan"}
+                    options={optionsCarePlane}
+                    onChange={handleSelectCaraPlan}
+                    styles={customStyles}
+                    value={typeCaraPlan}
+                  />
+                </div>
+                <div className="reportTypeSelector">
+                  <Select
+                    components={{ DropdownIndicator }}
+                    placeholder={"Frequency"}
+                    options={optionsFrequency}
+                    onChange={handleSelectFrequency}
+                    styles={customStyles}
+                    value={typeFrequency}
+                  />
+                </div>
+                <div className="reportTypeSelector">
+                  <Select
+                    components={{ DropdownIndicator }}
+                    placeholder={"Progress Test"}
+                    options={optionsProgressTest}
+                    onChange={handleSelectProgressTest}
+                    styles={customStyles}
+                    value={typeProgressTest}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
 
       <div className="containerViewReport_toggleBtn">
         <div
           onClick={handleChangeBtn}
-          className={activeBtn == "Brain" ? "activeBtn" : "btn"}
+          className={activeBtn === "Brain" ? "activeBtn" : "btn"}
         >
           Brain
         </div>
         <div
           onClick={handleChangeBtn}
-          className={activeBtn == "Brainwaves" ? "activeBtn" : "btn"}
+          className={activeBtn === "Brainwaves" ? "activeBtn" : "btn"}
         >
           Brainwaves
         </div>
         <div
           onClick={handleChangeBtn}
-          className={activeBtn == "HRV" ? "activeBtn" : "btn"}
+          className={activeBtn === "HRV" ? "activeBtn" : "btn"}
         >
           HRV
         </div>
         <div
           onClick={handleChangeBtn}
-          className={activeBtn == "Care plan" ? "activeBtn" : "btn"}
+          className={activeBtn === "Care plan" ? "activeBtn" : "btn"}
         >
           Care plan
         </div>
--- src/components/Reports/Reports.tsx
+++ src/components/Reports/Reports.tsx
@@ -1,14 +1,13 @@
-import React, { ReactElement, useEffect, useState } from 'react';
-import NavBar from '../NavBar/NavBar';
-import { CSVDownload, CSVLink } from "react-csv";
+import React, { ReactElement, useEffect, useState } from "react";
+import NavBar from "../NavBar/NavBar";
+import { CSVLink } from "react-csv";
 import DatePicker from "react-datepicker";
-import { ReactComponent as Arrow } from '../../images/arrow.svg'
+import { ReactComponent as Arrow } from "../../images/arrow.svg";
 import "react-datepicker/dist/react-datepicker.css";
-import './reports.sass'
-import Select, { components } from 'react-select'
-import { reportApi } from '../../api/reportApi';
-import { instance } from '../../api/axiosInstance';
-
+import "./reports.sass";
+import Select, { components } from "react-select";
+import { reportApi } from "../../api/reportApi";
+import { instance } from "../../api/axiosInstance";
 
 interface IDataReport {
   startDate: Date;
@@ -23,64 +22,64 @@ interface IDataReportToBack {
 }
 
 const customStyles: any = {
-  option: (provided: any, state: { isSelected: any; }) => ({
+  option: (provided: any, state: { isSelected: any }) => ({
     ...provided,
-    border: '1px solid #fff',
-    background: 'black',
-    fontSize: '25px',
-    color: 'white',
+    border: "1px solid #fff",
+    background: "black",
+    fontSize: "25px",
+    color: "white",
     padding: 20,
   }),
 
   control: () => ({
-    position: 'relative',
+    position: "relative",
     width: 290,
-    height: 61
+    height: 61,
   }),
 
   menu: () => ({
-    background: 'black',
-    marginTop: '-12px',
+    background: "black",
+    marginTop: "-12px",
   }),
 
   valueContainer: () => ({
-    position: 'relative',
-    border: '1px solid #aef7ff',
-    fontSize: '25px',
-    color: '#aef7ff',
-    borderRadius: '10px',
-    zIndex: '100',
-    background: 'black',
+    position: "relative",
+    border: "1px solid #aef7ff",
+    fontSize: "25px",
+    color: "#aef7ff",
+    borderRadius: "10px",
+    zIndex: "100",
+    background: "black",
   }),
 
   indicatorsContainer: () => ({
-    position: 'absolute',
-    zIndex: '1000',
-    top: '20px',
-    right: '20px'
+    position: "absolute",
+    zIndex: "1000",
+    top: "20px",
+    right: "20px",
   }),
 
   input: () => ({
     height: 61,
-    paddingLeft: '20px',
-    lineHeight: '61px'
+    paddingLeft: "20px",
+    lineHeight: "61px",
   }),
 
   placeholder: () => ({
-    paddingLeft: '20px',
-    position: 'absolute',
-    lineHeight: '61px'
+    paddingLeft: "20px",
+    position: "absolute",
+    lineHeight: "61px",
   }),
 
-  singleValue: (provided: any, state: { isDisabled: any; }) => {
-    const transition = 'opacity 300ms';
-    const color = '#aef7ff';
-    const fontSize = '25px';
-    const paddingLeft = '20px';
+  singleValue: (provided: any, state: { isDisabled: any }) => {
+    const transition = "opacity 300ms";
+    const color = "#aef7ff";
+    const fontSize = "25px";
+    const paddingLeft = "20px";
 
     return { ...provided, transition, color, fontSize, paddingLeft };
-  }
-}
+  },
+};
 
 const DropdownIndicator = (props: any) => {
   return (
@@ -99,35 +98,39 @@ export default function Reports(): ReactElement {
   const [isOpenModal, setIsOpenModel] = useState(false);
 
   const options = [
-    { value: 'Visits', label: 'Visits' },
-    { value: 'New_clients', label: 'New clients' },
-    { value: 'Revenue', label: 'Revenue' },
-    { value: 'Client_export', label: 'Client export' },
-    { value: 'Client_import', label: 'Client import' }
+    { value: "Visits", label: "Visits" },
+    { value: "New_clients", label: "New clients" },
+    { value: "Revenue", label: "Revenue" },
+    { value: "Client_export", label: "Client export" },
+    { value: "Client_import", label: "Client import" },
   ];
 
   const getFileWithVisits = async () => {
     try {
-      const response = await instance()
-      .get('api/client/report_visit');
+      const response = await instance().get("api/client/report_visit");
       console.log("GET: csv file with visits => ", response);
       const data = await response.data;
       setFile(data);
     } catch (error: any) {
-      console.log('GET: error message (file visits)=>  ', new Error(error.message));
+      console.log(
+        "GET: error message (file visits)=>  ",
+        new Error(error.message)
+      );
       throw new Error(error.message);
     }
   };
 
   const getFileWithNewClients = async () => {
     try {
-      const response = await instance()
-      .get('api/client/report_new_clients');
+      const response = await instance().get("api/client/report_new_clients");
       console.log("GET: csv file with new_clients => ", response);
-      const data = await response.data
+      const data = await response.data;
       setFile(data);
     } catch (error: any) {
-      console.log('GET: error message (file new_clients)=>  ', new Error(error.message));
+      console.log(
+        "GET: error message (file new_clients)=>  ",
+        new Error(error.message)
+      );
       throw new Error(error.message);
     }
   };
@@ -139,13 +142,20 @@ export default function Reports(): ReactElement {
       endDate: endDate,
     };
 
-    const dateStart = new Date(data.startDate.toString().replace(/GMT.*$/,'GMT+0000')).toISOString();
-    const fullStartDate = dateStart.replace("T", " ").replace(".", " ").split(" ");
+    const dateStart = new Date(
+      data.startDate.toString().replace(/GMT.*$/, "GMT+0000")
+    ).toISOString();
+    const fullStartDate = dateStart
+      .replace("T", " ")
+      .replace(".", " ")
+      .split(" ");
     const dStart = fullStartDate[0].split("-");
     const fullTime = fullStartDate[1];
     const startDateToBack = `${dStart[1]}/${dStart[2]}/${dStart[0]}, ${fullTime}`;
 
-    const dateEnd = new Date(data.endDate.toString().replace(/GMT.*$/,'GMT+0000')).toISOString();
+    const dateEnd = new Date(
+      data.endDate.toString().replace(/GMT.*$/, "GMT+0000")
+    ).toISOString();
     const fullEndDate = dateEnd.replace("T", " ").replace(".", " ").split(" ");
     const dEnd = fullEndDate[0].split("-");
     const fullTimeEnd = fullEndDate[1];
@@ -159,14 +169,14 @@ export default function Reports(): ReactElement {
       endDate: endDateToBack,
     };
 
-    if (dataToBack.type === 'visits') {
+    if (dataToBack.type === "visits") {
       reportApi.filterDateToReportVisit(dataToBack);
-    };
+    }
 
-    if (dataToBack.type === 'new_clients') {
+    if (dataToBack.type === "new_clients") {
       reportApi.filterDateToReportNewClients(dataToBack);
-    };
-  }
+    }
+  };
 
   useEffect(() => {
     setType(type);
@@ -174,33 +184,31 @@ export default function Reports(): ReactElement {
     setEndDate(endDate);
     setFile(file);
     if (startDate && endDate) {
-        filterDateForReport()
-    };
+      filterDateForReport();
+    }
   }, [type, startDate, endDate, file]);
 
   useEffect(() => {
-    setIsOpenModel(isOpenModal)
+    setIsOpenModel(isOpenModal);
   }, [isOpenModal]);
 
   const handleSubmit = () => {
-    if (type && type.value.toLowerCase() === 'visits') {
+    if (type && type.value.toLowerCase() === "visits") {
       getFileWithVisits();
-    };
-    if (type && type.value.toLowerCase() === 'new_clients') {
+    }
+    if (type && type.value.toLowerCase() === "new_clients") {
       getFileWithNewClients();
-    };
+    }
     setIsOpenModel(true);
   };
 
   const handleSelect = (type: string) => {
-    setType(type)
+    setType(type);
   };
   if (file) {
     console.log("file", file.length);
     console.log("type.value", type.value);
-  };
-
-
+  }
 
   return (
     <>
@@ -211,7 +219,7 @@ export default function Reports(): ReactElement {
           <div className="reportTypeSelector">
             <Select
               components={{ DropdownIndicator }}
-              placeholder={'Select type'}
+              placeholder={"Select type"}
               options={options}
               onChange={handleSelect}
               styles={customStyles}
@@ -246,63 +254,79 @@ export default function Reports(): ReactElement {
                 placeholderText="End date"
               />
             </div>
-                <button
-                  onClick={handleSubmit}
-                  className={`${!type || !startDate || !endDate ? "reportsButtonDisabled" : "reportsButton"}`}
-                  disabled={!type || !startDate || !endDate}>
-                    <span className="text">Generate</span>
-                </button>
+            <button
+              onClick={handleSubmit}
+              className={`${
+                !type || !startDate || !endDate
+                  ? "reportsButtonDisabled"
+                  : "reportsButton"
+              }`}
+              disabled={!type || !startDate || !endDate}
+            >
+              <span className="text">Generate</span>
+            </button>
           </div>
         </div>
       </div>
 
       <div className={isOpenModal ? "modalOpen" : "modal"}>
         <div className="modal-content">
-            <div className="close" onClick={() => setIsOpenModel(!isOpenModal)}>&times;</div>
+          <div className="close" onClick={() => setIsOpenModel(!isOpenModal)}>
+            &times;
+          </div>
 
-              { type &&
-                type.value.toLowerCase() === 'visits' ?
-                  <div className="modalText">
-                    { file && file.length > 42
-                      ? `Download a report of visits for this period`
-                      : <div className="modalTextError">
-                          There are no visits during this period
-                        </div>
-                    }
-                  </div>
-                    :
-                  type &&
-                  type.value.toLowerCase() === 'new_clients' ?
-                    <div className="modalText">
-                      { file && file.length > 132
-                        ? `Download a report of new clients for this period`
-                        : <div className="modalTextError">
-                            There are no new clients during this period
-                          </div>
-                      }
-                    </div> : ""
-              }
-            {
-                ((file  && file.length > 42 && type.value.toLowerCase() === "visits")
-                  ||
-                (file && file.length > 132 && type.value.toLowerCase() === "new_clients"))
-                  &&
-                <div className="btnsModal">
-                  <div className="btnModalOk" onClick={() => setIsOpenModel(!isOpenModal)}>
-                    <CSVLink
-                      className="csvLink"
-                      data={file}
-                      filename={`${type.value.toLowerCase() + "_" + new Date().toLocaleDateString("en-US")}.csv`}
-                      target="_blank"
-                    >
-                    Ok
-                    </CSVLink>
-                  </div>
-                  <div onClick={() => setIsOpenModel(!isOpenModal)}>Cancel</div>
+          {type && type.value.toLowerCase() === "visits" ? (
+            <div className="modalText">
+              {file && file.length > 42 ? (
+                `Download a report of visits for this period`
+              ) : (
+                <div className="modalTextError">
+                  There are no visits during this period
                 </div>
-            }
+              )}
+            </div>
+          ) : type && type.value.toLowerCase() === "new_clients" ? (
+            <div className="modalText">
+              {file && file.length > 132 ? (
+                `Download a report of new clients for this period`
+              ) : (
+                <div className="modalTextError">
+                  There are no new clients during this period
+                </div>
+              )}
+            </div>
+          ) : (
+            ""
+          )}
+          {((file &&
+            file.length > 42 &&
+            type.value.toLowerCase() === "visits") ||
+            (file &&
+              file.length > 132 &&
+              type.value.toLowerCase() === "new_clients")) && (
+            <div className="btnsModal">
+              <div
+                className="btnModalOk"
+                onClick={() => setIsOpenModel(!isOpenModal)}
+              >
+                <CSVLink
+                  className="csvLink"
+                  data={file}
+                  filename={`${
+                    type.value.toLowerCase() +
+                    "_" +
+                    new Date().toLocaleDateString("en-US")
+                  }.csv`}
+                  target="_blank"
+                >
+                  Ok
+                </CSVLink>
+              </div>
+              <div onClick={() => setIsOpenModel(!isOpenModal)}>Cancel</div>
+            </div>
+          )}
         </div>
       </div>
     </>
-  )
+  );
 }
