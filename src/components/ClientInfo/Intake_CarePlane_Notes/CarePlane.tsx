1afd70c49b0ff3387c9bde6fd727028ec971c40f
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-12-01 16:35:22 +0000

    get carePlanInfo

--- src/components/ClientInfo/Intake_CarePlane_Notes/CarePlane.tsx
+++ src/components/ClientInfo/Intake_CarePlane_Notes/CarePlane.tsx
@@ -14,6 +14,16 @@ export function CarePlane(props: { activeBtnRogueMode: string }): ReactElement {
   console.log("Care Plane -> api_key", api_key);
   const [client, setClient] = useState<Client>(ClientDefault);
 
+  const [carePlanInfo, setCarePlanInfo] = useState({
+    first_visit: "",
+    last_visit: "",
+    total_visits: "",
+    care_plan_length: "",
+    visit_frequency: "",
+    next_visit: "",
+    expiration: "",
+  });
+
   const getClient = async () => {
     try {
       const response = await instance().get(
@@ -32,8 +42,29 @@ export function CarePlane(props: { activeBtnRogueMode: string }): ReactElement {
     }
   };
 
+  const getCarePlanInfo = async () => {
+    try {
+      const response = await instance().get(
+        `api/test/info_for_care_plan_page/${api_key}`
+      );
+      console.log("GET: getCarePlanInfo => ", response.data);
+      setCarePlanInfo(response.data);
+      return response.data;
+    } catch (error: any) {
+      console.log("GET: error message getCarePlanInfo =>  ", error.message);
+      console.log(
+        "error response data getCarePlanInfo => ",
+        error.response.data
+      );
+      throw new Error(error.message);
+    }
+  };
+
+  console.log("CarePlane: carePlanInfo => ", carePlanInfo);
+
   useEffect(() => {
     getClient();
+    getCarePlanInfo();
   }, [api_key]);
 
   return (
@@ -49,38 +80,38 @@ export function CarePlane(props: { activeBtnRogueMode: string }): ReactElement {
             <div className="carePlaneInfo">
               <div className="carePlaneInfo_item">
                 <div className="title">First Visit:</div>
-                <div className="text"></div>
+                <div className="text">{carePlanInfo.first_visit}</div>
               </div>
 
               <div className="carePlaneInfo_item">
                 <div className="title">Last Visit:</div>
-                <div className="text"></div>
+                <div className="text">{carePlanInfo.last_visit}</div>
               </div>
 
               <div className="carePlaneInfo_item">
                 <div className="title">Total Visits:</div>
-                <div className="text"></div>
+                <div className="text">{carePlanInfo.total_visits}</div>
               </div>
 
               <div className="carePlaneInfo_item">
                 <div className="title">Care plan length:</div>
-                <div className="text"></div>
+                <div className="text">{carePlanInfo.care_plan_length}</div>
               </div>
 
               <div className="carePlaneInfo_item">
                 <div className="title">Visit Frequency:</div>
-                <div className="text"></div>
+                <div className="text">{carePlanInfo.visit_frequency}</div>
               </div>
 
               <div className="carePlaneInfo_item">
                 <div className="title">Next visit:</div>
-                <div className="text"></div>
+                <div className="text">{carePlanInfo.next_visit}</div>
               </div>
 
-              <div className="carePlaneInfo_item">
+              {/* <div className="carePlaneInfo_item">
                 <div className="title">Expiration: </div>
-                <div></div>
-              </div>
+                <div>{carePlanInfo.expiration}</div>
+              </div> */}
             </div>
           </div>
           <div className="intakeInfoText_results">
