3a7cdb389f5eae43e0bd6305022706b848dd7aa2
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-12-20 15:32:40 +0000

    Arousal

--- src/components/ClientInfo/Dashboard/Arousal.tsx
+++ src/components/ClientInfo/Dashboard/Arousal.tsx
@@ -4,43 +4,61 @@ import "@progress/kendo-theme-default/dist/all.css";
 import {
   Chart,
   ChartArea,
+  ChartCategoryAxis,
+  ChartCategoryAxisItem,
+  ChartLegend,
   ChartSeries,
   ChartSeriesItem,
+  ChartSeriesLabels,
+  ChartTitle,
 } from "@progress/kendo-react-charts";
 import "hammerjs";
 
 export default function Arousal(): ReactElement {
-  const data1 = [8];
-  const data2 = [13];
+  const data1 = [13];
+  const data2 = [8];
+
+  const categories = ["S", "P"];
+  const series = [
+    {
+      data: [13, 8],
+      color: "#3F8DAF",
+    },
+  ];
 
   return (
     <div className="containerDashboard">
       <div className="title">Arousal</div>
       <Chart>
         <ChartArea background={"transparent"} />
+
+        <ChartTitle text="" />
+        <ChartLegend position="top" orientation="horizontal" />
+        <ChartCategoryAxis>
+          <ChartCategoryAxisItem categories={categories} startAngle={45} />
+        </ChartCategoryAxis>
+
+        {/* <ChartCategoryAxisItem categories={categories} startAngle={45} /> */}
+
         <ChartSeries>
-          <ChartSeriesItem
-            data={data1}
-            type="column"
-            field="count"
-            // categoryField="date"
-            aggregate="count"
-            // stack={true}
-            // opacity={0.7}
-            // gap={0.06}
-            color={"#A3CEC7"}
-          />
-          <ChartSeriesItem
-            data={data2}
-            type="column"
-            field="count"
-            // categoryField="date"
-            aggregate="count"
-            // stack={true}
-            // opacity={0.7}
-            // gap={0.06}
-            color={"#3F8DAF"}
-          />
+          {series.map((data, index) => {
+            return (
+              <ChartSeriesItem
+                key={index}
+                data={data.data}
+                type="column"
+                field="count"
+                aggregate="count"
+                color={data.color}
+
+                // dynamicHeight={false}
+                // dynamicSlope={false}
+                // visible={true}
+              >
+                <ChartSeriesLabels color="#fff" background="none" />
+              </ChartSeriesItem>
+            );
+          })}
         </ChartSeries>
       </Chart>
     </div>
