31b447b437bf4298ab5587f8f33d45d0cb349fbb
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-12-21 10:57:44 +0000

    ChartComponent

--- src/components/ClientInfo/Dashboard/Arousal.tsx
+++ src/components/ClientInfo/Dashboard/Arousal.tsx
@@ -2,34 +2,86 @@ import React, { ReactElement } from "react";
 import "./dashboard.sass";
 import "@progress/kendo-theme-default/dist/all.css";
 import {
-  Chart,
-  ChartArea,
-  ChartCategoryAxis,
-  ChartCategoryAxisItem,
-  ChartLegend,
-  ChartSeries,
-  ChartSeriesItem,
-  ChartSeriesLabels,
-  ChartTitle,
-} from "@progress/kendo-react-charts";
-import "hammerjs";
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  Legend,
+  Category,
+  Tooltip,
+  DataLabel,
+  ColumnSeries,
+  AxisModel,
+} from "@syncfusion/ej2-react-charts";
+// import {
+//   Chart,
+//   ChartArea,
+//   ChartCategoryAxis,
+//   ChartCategoryAxisItem,
+//   ChartLegend,
+//   ChartSeries,
+//   ChartSeriesItem,
+//   ChartSeriesLabels,
+//   ChartTitle,
+// } from "@progress/kendo-react-charts";
+// import "hammerjs";
 
 export default function Arousal(): ReactElement {
-  const data1 = [13];
-  const data2 = [8];
+  // const data1 = [13];
+  // const data2 = [8];
 
-  const categories = ["S", "P"];
-  const series = [
-    {
-      data: [13, 8],
-      color: "#3F8DAF",
-    },
+  // const categories = ["S", "P"];
+  // const series = [
+  //   {
+  //     data: [13, 8],
+  //     color: "#3F8DAF",
+  //   },
+  // ];
+
+  const data = [
+    { name: "S", value: 75 },
+    { name: "P", value: 35 },
   ];
+
+  const pointRender = (args: any) => {
+    let seriesColor = ["#982594", "#15819B"];
+    args.fill = seriesColor[args.point.index];
+  };
+
+  const primaryxAxis: AxisModel | undefined = {
+    valueType: "Category",
+  };
+  const primaryyAxis = {
+    minimum: 0,
+    maximum: 100,
+    interval: 15,
+    title: "",
+  };
 
   return (
     <div className="containerDashboard">
       <div className="title">Arousal</div>
-      <Chart>
+      <ChartComponent
+        id="charts"
+        primaryXAxis={primaryxAxis}
+        primaryYAxis={primaryyAxis}
+        pointRender={pointRender}
+        width="280"
+        height="360"
+        background="none"
+      >
+        <Inject services={[ColumnSeries, Tooltip, DataLabel, Category]} />
+        <SeriesCollectionDirective>
+          <SeriesDirective
+            dataSource={data}
+            xName="name"
+            yName="value"
+            name="Gold"
+            type="Column"
+          ></SeriesDirective>
+        </SeriesCollectionDirective>
+      </ChartComponent>
+      {/* <Chart>
         <ChartArea background={"transparent"} />
 
         <ChartTitle text="" />
@@ -37,8 +89,6 @@ export default function Arousal(): ReactElement {
         <ChartCategoryAxis>
           <ChartCategoryAxisItem categories={categories} startAngle={45} />
         </ChartCategoryAxis>
-
-        {/* <ChartCategoryAxisItem categories={categories} startAngle={45} /> */}
 
         <ChartSeries>
           {series.map((data, index) => {
@@ -50,17 +100,13 @@ export default function Arousal(): ReactElement {
                 field="count"
                 aggregate="count"
                 color={data.color}
-
-                // dynamicHeight={false}
-                // dynamicSlope={false}
-                // visible={true}
               >
                 <ChartSeriesLabels color="#fff" background="none" />
               </ChartSeriesItem>
             );
           })}
         </ChartSeries>
-      </Chart>
+      </Chart> */}
     </div>
   );
 }
