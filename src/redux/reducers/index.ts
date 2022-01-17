243e6083a677f431ac8e9812c2dafa60df82048e
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2022-01-17 08:04:40 +0000

    add to redux status stripe

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import { authReducer } from "./authReducer";
 import { commonReducer } from "./commonReducer";
 import { dashboardReducer } from "./dashboardReducer";
+import { stripeReducer } from "./stripeReducer";
 
 export const rootReducer = combineReducers({
   app: commonReducer,
   auth: authReducer,
   dashboard: dashboardReducer,
+  stripe: stripeReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
